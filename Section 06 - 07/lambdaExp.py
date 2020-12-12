683641abbd338cfd2f70e4224ddd0b4e076f5962
Enver Gul <envergul23@yahoo.com>
2020-12-12 09:09:43 +0300

    Lambda function is added and purfunctions are updated

--- Section 06 - 07/lambdaExp.py
+++ Section 06 - 07/lambdaExp.py
@@ -1,21 +1,17 @@
-#map, filter, zip, reduce ==> important pure function 
-from functools import reduce #import reduce function
+#lambda expression
+from functools import reduce 
 
 mylist = [1 ,2 ,3 ]
 yrlist = [10,20,30]
 #MAP Function
-def multiply_by_2(li):
-    new_list=[]
-    for i in li:
-        new_list.append(i*2)
-    return new_list
-
 def multiply_by_3(item):
     return item*3
 
 print("Multiply with 3: ",list(map(multiply_by_3,mylist)))
 print("Original List  : ",mylist)
 
+print("Lambda multiply with 4: ",list(map(lambda item: item*4,mylist)))
+print("Original List  : ",mylist)
 #FILTER Function
 def only_odd(item):
     return item % 2 !=0

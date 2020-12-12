bc8253feae472971ab53cd23e941b9c1db3dde70
Enver Gul <envergul23@yahoo.com>
2020-12-12 09:28:30 +0300

    Lambda function files are updated

--- Section 06 - 07/lambdaExp.py
+++ Section 06 - 07/lambdaExp.py
@@ -8,20 +8,21 @@ def multiply_by_3(item):
     return item*3
 
 print("Multiply with 3: ",list(map(multiply_by_3,mylist)))
-print("Original List  : ",mylist)
-
 print("Lambda multiply with 4: ",list(map(lambda item: item*4,mylist)))
-print("Original List  : ",mylist)
+print("Original List  : ",mylist,"\n")
+
+
 #FILTER Function
 def only_odd(item):
     return item % 2 !=0
 
 print("Filter only odds: ",list(filter(only_odd,mylist)))
-print("Original List  : ",mylist)
+print("Lambda Filter only odds: ",list(filter(lambda item: item % 2 !=0 ,mylist)))
+print("Original List  : ",mylist,"\n")
 
 #ZIP Function ==> it creates new tuples
 print("Zip two list  : ",list(zip(mylist,yrlist)))
-print("Original Lists: ",mylist, yrlist)
+print("Original Lists: ",mylist, yrlist,"\n")
 
 #REDUCE Function
 def accumulator(acc, item):
@@ -29,4 +30,13 @@ def accumulator(acc, item):
     return acc + item
 
 print("Reduce the List: ",reduce(accumulator,mylist,0))
-print("Original Lists: ",mylist)
+print("Lambda Reduce the List: ",reduce(lambda acc, item: acc+item ,mylist,0))
+print("Original List  : ",mylist,"\n")
+
+print("Square each item:",list(map(lambda item:item**2,mylist)))
+print("Original List  : ",mylist,"\n")
+
+a= [(0,2),(4,3),(10,-1),(9,9)]
+print("Original List  : ",a)
+a.sort(key=lambda x:x[1])
+print("Sort by 2nd Key:",a,"\n")
